64f7e3cb5187bd9310c89984a04b6f83d1b53983
Cole Moses <mosescj@colostate.edu>
2023-04-20 12:41:54 -0600

    Updated files for setup

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
    entry: path.join(__dirname, 'src/js', 'index.js'), // Our frontend will be inside the src folder
    output: {
       path: path.join(__dirname, 'dist'),
-      filename: 'build.js' // The final file will be created in dist/build.js
+      filename: 'run.js' // The final file will be created in dist/build.js
    },
    module: {
       rules: [{
