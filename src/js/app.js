928999721d5f9eac04f12f22b7c6027cbffcf56c
ThomasWittich <wittichta@gmail.com>
2023-04-20 13:27:59 -0600

    c

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+import React from "react";
+import Web3 from "web3";
+
+import "./css/app.css";
---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import app from "app";
+
+ReactDOM.render(<app />, document.getElementById("reactStuff"));
