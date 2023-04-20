24e5e06fe98d4228ed89e8f02659faa358da7dda
ThomasWittich <wittichta@gmail.com>
2023-04-20 13:42:05 -0600

    c

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,14 @@ import React from "react";
 import Web3 from "web3";
 
 import "./css/app.css";
+
+
+class app extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.conttractAddress = "";//contract address here;
+
+        //initialize bets allowed here
+    };
+}
