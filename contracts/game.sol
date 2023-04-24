ccfd475b7c31c6e129b245bf9bc21cbbf8c6629a
Cole Moses <mosescj@colostate.edu>
2023-04-24 09:58:05 -0600

    added some of the contract

---
+++ contracts/game.sol
@@ -0,0 +1,52 @@
+pragma solidity ^0.8.19;
+
+
+contract Casino {
+
+    address  public house;
+    uint minBet = 100;
+    uint totalBets = 0;
+    address[] public players ;
+    uint totalWinners = 0;
+    uint payout = 0;
+
+    struct Player{
+        uint bet;
+        //placeholder for choice
+        uint choice;
+        bool winnerFlag;
+    }
+
+    mapping(address => Player) public playerInputs;
+    
+    constructor() {
+        house = msg.sender;
+    }
+
+    function bet(uint choice) public payable{
+        require(msg.value >= minBet);
+        playerInputs[msg.sender].bet = msg.value;
+        playerInputs[msg.sender].choice = choice;
+        playerInputs[msg.sender].winnerFlag = false;
+        totalBets += msg.value;
+        players.push(msg.sender);
+    }
+
+    function winner(uint choice)public {
+        for(uint i = 0; i < players.length; i++){
+            
+            if(playerInputs[players[i]].choice == choice){
+                playerInputs[players[i]].winnerFlag = true;
+                totalWinners++;
+            }
+        }
+        payout = totalBets/totalWinners;
+        for(uint i = 0; i < players.length; i++){
+            address payable playerAdd = players[i];
+            if(playerInputs[players[i]].winnerFlag && players[i] != address(0))
+                players[i].transfer(payout);
+        }
+        
+    }   
+
+}
