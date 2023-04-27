af1b525a3fe496f8ed146931fb4664aa0013d6bf
ThomasWittich <wittichta@gmail.com>
2023-04-27 12:42:36 -0600

    c

--- src/js/index.js
+++ src/js/index.js
@@ -5,25 +5,4 @@ import './../css/index.css'
 import app from "app"
 import { render } from 'express/lib/response'
 
-class App extends React.Component {
-    constructor (props) {
-        super (props)
-        this.state = {
-            lastWinner : 0,
-            timer : 0
-        }
-    }
-    voteNumber(number){
-        console.log(number)
-     } render() {
-        return (
-            <div className="short-description">
-                <h1> 
-                    A Chance to Win a Ton of Money When You Place Ethereum Bets!
-                </h1>
-            </div>
-        )
-     }
-}
-
 ReactDOM.render(<app />, document.getElementById("reactStuff"));
