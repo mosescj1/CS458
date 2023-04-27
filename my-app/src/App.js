a18763672a49499d646c83a7041d71e560066bb9
ThomasWittich <wittichta@gmail.com>
2023-04-27 13:28:44 -0600

    c

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Web3 from "web3";
 
-import "./css/app.css";
+import "./css/App.css";
 
 const INFURA_KEY = "key from infura";
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'reaact-dom'
 import Web3 from 'web3'
-import './../css/index.css'
+import './../css/Index.css'
 import app from "app"
 import { render } from 'express/lib/response'
 
-ReactDOM.render(<app />, document.getElementById("reactStuff"));
+ReactDOM.render(<App />, document.getElementById("reactStuff"));
