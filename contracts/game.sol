e3c781d740aba902300b64c9e3c0a3fdbf967318
Cole Moses <mosescj@colostate.edu>
2023-04-27 20:49:51 -0600

    updated js and the vars in contract

--- contracts/game.sol
+++ contracts/game.sol
@@ -4,11 +4,12 @@ pragma solidity ^0.8.19;
 contract Casino {
 
     address public owner;
-    uint minBet = 100;
+    uint constant minBet = 100;
     uint totalBets = 0;
     address[] public players ;
     uint payout = 0;
     uint numOfBets = 0;
+    string winningChoice;
 
     struct Player{
         uint bet;
@@ -57,6 +58,8 @@ contract Casino {
 
     function generateRandom() private {
         uint256 win= (block.number %10 * block.timestamp%5) %2;
+        if(win == 0) winningChoice = "Heads";
+        if(win == 1) winningChoice = "Tails";
         winner(win);
     }
 
